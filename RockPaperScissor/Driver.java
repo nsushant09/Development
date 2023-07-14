95b8ee05fc575d6351a9820afb2864aca0849877
Sushant Neupane <nsushant09@gmail.com>
2023-07-14 21:28:22 +0545

    RockPaperScissor

--- RockPaperScissor/Driver.java
+++ RockPaperScissor/Driver.java
@@ -7,30 +7,16 @@ public class Driver {
     private final static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Element userValue = ElementFactory.getElement(getUserValue());
-        Element automatedValue = ElementFactory.getElement(getAutomatedValue());
+
+        ValueGenerator valueGenerator = new ValueGenerator();
+        Element userValue = ElementFactory.getElement(getElementName(valueGenerator.getUserValue()));
+        Element automatedValue = ElementFactory.getElement(getElementName(valueGenerator.getAutomatedValue()));
 
         Result result = userValue.getResult(automatedValue);
 
         System.out.println("You chose : " + userValue.getName());
         System.out.println("System chose : " + automatedValue.getName());
         displayResult(result);
-    }
-
-    public static String getUserValue() {
-        System.out.println("Enter your choice (1-3) : ");
-        System.out.println("1. Rock");
-        System.out.println("2. Paper");
-        System.out.println("3. Scissor");
-        System.out.println("Your choice : ");
-
-        int input = sc.nextInt();
-        return getElementName(input);
-    }
-
-    public static String getAutomatedValue() {
-        int input = (int) (1 + Math.random() * (3 - 1));
-        return getElementName(input);
     }
 
     public static String getElementName(int input) {
