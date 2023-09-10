c28f1168b5a4bc70f0ea8375523d8b3eacf4d62d
Sushant Neupane <nsushant09@gmail.com>
2023-09-10 21:31:46 +0545

    Top K Frequent Element

---
+++ DSA/arrays_and_hashing/TopKFrequentElement.kt
@@ -0,0 +1,28 @@
+package DSA.arrays_and_hashing
+
+import java.util.*
+import kotlin.collections.HashMap
+
+fun main(){
+    println(topKFrequent(intArrayOf(4, 1, -1, 2, -1, 2, 3), 2).contentToString())
+}
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+    var map = hashMapOf<Int, Int>()
+    for (num in nums) {
+        if (map.containsKey(num)) {
+            map.replace(num, map[num]!! + 1)
+        } else {
+            map[num] = 1
+        }
+    }
+
+    map = map.entries
+        .sortedByDescending { it.value }
+        .associate { it.key to it.value } as HashMap<Int, Int>
+
+    val output = IntArray(k)
+    for(i in 0 until k){
+        output[i] = map.keys.elementAt(i)
+    }
+    return output
+}
