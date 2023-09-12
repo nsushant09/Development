7d3aae3a08e7d8fd60c23061903101b7740e1cfa
Sushant Neupane <nsushant09@gmail.com>
2023-09-12 19:03:59 +0545

    Three sum

--- DSA/arrays_and_hashing/TwoSum.java
+++ DSA/arrays_and_hashing/TwoSum.java
@@ -11,34 +11,40 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             hashMap.put(nums[i], i);
         }
 
-        for (int i = 0; i < nums.length; i++){
-            if(hashMap.containsKey(target - nums[i])){
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
                 int newIndex = hashMap.get(target - nums[i]);
-                if(i != newIndex)
+                if (i != newIndex)
                     return new int[]{i, hashMap.get(target - nums[i])};
             }
         }
 
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
 
-    public int[] threeSum(int[] nums, int target){
+    public boolean threeSum(int[] nums, int target) {
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
+
+        for (int i = 0; i < nums.length; i++) {
             int low = i + 1;
             int high = nums.length - 1;
 
-            while(low < high){
+            while (low < high) {
                 int current = nums[i] + nums[low] + nums[high];
-                if(current == target)
-                    return
+                if (current == target)
+                    return true;
+                if (current < target)
+                    low++;
+                if (current > target)
+                    high++;
             }
 
         }
-        return new int[] {-1,-1};
+
+        return false;
     }
 }
